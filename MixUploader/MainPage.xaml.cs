4cc344b3f92a89415f0fc51c3df1d50eed07ec78
Charlton Reid <icemanwts@hotmail.co.uk>
2020-10-14 23:20:08 +0100

    Add project files.

---
+++ MixUploader/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Win32;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.Popups;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MixUploader
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openpicker = new FileOpenPicker();
+            openpicker.ViewMode = PickerViewMode.Thumbnail;
+            openpicker.FileTypeFilter.Add(".mp3");
+            openpicker.FileTypeFilter.Add(".wav");
+
+            StorageFile file = await openpicker.PickSingleFileAsync();
+            if(file != null)
+            {
+                //adds file path selected to the browse textbox then turns that into byte array.
+                //once that is done it assigns the file to UploadSite static member
+                try
+                {
+
+                    FileLocationTextBox.Text = file.Path;
+
+                    var filestream = await file.OpenStreamForReadAsync();
+                    byte[] fileBytes = new byte[filestream.Length];
+
+                    filestream.Read(fileBytes, 0, fileBytes.Length);
+                    
+                    //var Uploadfile = new UploadSite();
+                    //await Uploadfile.UploadToMixcloudAsync(fileBytes);
+
+
+
+                    
+
+                    UploadSite.audiofile = fileBytes;
+                    UploadSite.filename = file.Name;
+
+                    filestream.Close();
+
+
+                    //byte[] readbytes = filestream;
+
+                    //await filestream.ReadAllBytesAsync(FileLocationTextBox.Text);
+
+                    //UploadSite.audiofile = filestream;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+
+
+
+        }
+
+        private async void SendFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            UploadSite.UploadTitle = UploadTitleTextbox.Text;
+
+            UploadSite post = new UploadSite();
+
+            if (MixcloudCheckbox.IsChecked == true)
+            {
+                await post.UploadToMixcloudAsync(UploadSite.audiofile);
+
+                string responsebody = post.responsebody;
+
+                var msg = new MessageDialog(responsebody);
+
+                await msg.ShowAsync();
+
+            }
+
+            if (SoundcloudCheckbox.IsChecked == true)
+            {
+                await post.UploadToSoundcloudAsync(UploadSite.audiofile);
+
+                string responsebody = post.responsebody;
+
+                var msg = new MessageDialog(responsebody);
+
+                await msg.ShowAsync();
+
+            }
+        }
+    }
+}
---
+++ MixUploader/UploadSite.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Globalization;
+using System.IO;
+using System.Net.Http.Headers;
+using Windows.Web.Http.Headers;
+using Windows.UI.Popups;
+
+namespace MixUploader
+{
+    public class UploadSite
+    {
+        public string responsebody { get; set; }
+
+        public static string UploadTitle { get; set; }
+
+        public static byte[] audiofile { get; set; }
+
+        public static string filename { get; set; }
+
+        public static string MixcloudAuthCode { get; set; } = "3aLMAp4mbrN5SSb5sp4tAcP3sq7naRck";
+
+        public static long MixcloudMaxBytes { get; set; } = 4294967296;
+
+        public static string SoundcloudAuthCode { get; set; } = "3aLMAp4mbrN5SSb5sp4tAcP3sq7naRck";
+
+
+        public async Task UploadToMixcloudAsync(byte[] file)
+        {
+            try
+            {
+                /*Live Mixcloud URL*/
+                //Uri uri = new Uri($"https://api.mixcloud.com/upload/?access_token={MixcloudAuthCode}");
+
+                /*Test Dump URL*/
+                Uri uri = new Uri($"http://ptsv2.com/t/vmygd-1602012853/post?access_token={MixcloudAuthCode}");
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+                    client.DefaultRequestHeaders.AcceptEncoding.TryParseAdd("gzip");
+                    client.DefaultRequestHeaders.AcceptEncoding.TryParseAdd("deflate");
+                    client.DefaultRequestHeaders.AcceptEncoding.TryParseAdd("br");
+
+                    using (var formcontent = new MultipartFormDataContent("Upload----" /*+ DateTime.Now.ToString(CultureInfo.InvariantCulture)*/))
+                    {
+
+                        formcontent.Headers.ContentType.MediaType = "multipart/form-data";
+
+                        formcontent.Add(new ByteArrayContent(file, 0, file.Length), "mp3", filename);
+                        formcontent.Add(new StringContent(UploadTitle), "name");
+                        formcontent.Add(new StringContent("True"), "unlisted");
+
+                        HttpResponseMessage httpreponse = new HttpResponseMessage();
+
+                        if (Convert.ToInt64(file.Length) > MixcloudMaxBytes)
+                        {
+                            Console.WriteLine("Filesize too large");
+                            responsebody = "Mixcloud: Filesize too large";
+                        }
+                        else
+                        {
+                            using (var Upload = await client.PostAsync(uri, formcontent))
+                            {
+                                
+
+                                try
+                                {
+                                    Upload.EnsureSuccessStatusCode();
+
+                                    responsebody = "Mixcloud: Upload Successful";
+                                }
+                                catch (HttpRequestException ex)
+                                {
+                                    responsebody = $"{ex.Source} says {ex.Message} - Response: {Upload.ReasonPhrase.ToString()}";
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+ 
+        }
+
+        public async Task GetMixcloudAsync()
+        {
+            HttpClient client = new HttpClient();
+            Uri uri = new Uri("https://api.mixcloud.com/shoto_uk/in-the-lab-talks-1-with-nurtured-beatz-recordings-kalm-spindall/");
+            HttpResponseMessage httpreponse = new HttpResponseMessage();
+            httpreponse = await client.GetAsync(uri);
+            //httpreponse.EnsureSuccessStatusCode();
+            responsebody = await httpreponse.Content.ReadAsStringAsync();
+
+        }
+
+        public async Task UploadToSoundcloudAsync(byte[] file)
+        {
+            try
+            {
+                /*Live Mixcloud URL*/
+                //Uri uri = new Uri($"https://api.mixcloud.com/upload/?access_token={MixcloudAuthCode}");
+
+                /*Test Dump URL*/
+                Uri uri = new Uri($"http://ptsv2.com/t/vmygd-1602012853/post?access_token={MixcloudAuthCode}");
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+                    client.DefaultRequestHeaders.AcceptEncoding.TryParseAdd("gzip");
+                    client.DefaultRequestHeaders.AcceptEncoding.TryParseAdd("deflate");
+                    client.DefaultRequestHeaders.AcceptEncoding.TryParseAdd("br");
+
+                    using (var formcontent = new MultipartFormDataContent("Upload----" /*+ DateTime.Now.ToString(CultureInfo.InvariantCulture)*/))
+                    {
+
+                        formcontent.Headers.ContentType.MediaType = "multipart/form-data";
+
+                        formcontent.Add(new ByteArrayContent(file, 0, file.Length), "mp3", filename);
+                        formcontent.Add(new StringContent(filename), "name");
+                        formcontent.Add(new StringContent("True"), "unlisted");
+
+                        HttpResponseMessage httpreponse = new HttpResponseMessage();
+
+                        if (Convert.ToInt64(file.Length) > MixcloudMaxBytes)
+                        {
+                            Console.WriteLine("Filesize too large");
+                            responsebody = "Mixcloud: Filesize too large";
+                        }
+                        else
+                        {
+                            using (var Upload = await client.PostAsync(uri, formcontent))
+                            {
+                                try
+                                {
+                                    Upload.EnsureSuccessStatusCode();
+
+                                    responsebody = "Mixcloud: Upload Successful";
+                                }
+                                catch (HttpRequestException ex)
+                                {
+                                    responsebody = $"{ex.Source} says {ex.Message} - Response: {Upload.ReasonPhrase.ToString()}";
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
